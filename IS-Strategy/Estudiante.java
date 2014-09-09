8388aa016ee6056780c7f81e10468f01fd910eba
Ajax <alejandro.delgado@ucsp.edu.pe>
2014-09-08 22:45:18 -0500

    Añadiendo clase Estudiante
    Signed-off-by: Ajax <alejandro.delgado@ucsp.edu.pe>

---
+++ IS-Strategy/Estudiante.java
@@ -0,0 +1,12 @@
+
+public class Estudiante {
+	public String Nombre;
+	public Integer Codigo;
+	public Integer Nota;
+	
+	public Estudiante (String T_nombre, Integer T_codigo, Integer T_nota){
+		this.Nombre=T_nombre;
+		this.Codigo=T_codigo;
+		this.Nota=T_nota;
+	}
+}
