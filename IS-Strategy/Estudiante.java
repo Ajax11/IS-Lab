e18b45460c42f994cd03a3d62819798c40e6fbce
Alejandro Delgado <alejandro.delgado@ucsp.edu.pe>
2014-09-09 14:40:53 -0500

    Update Estudiante.java

--- IS-Strategy/Estudiante.java
+++ IS-Strategy/Estudiante.java
@@ -1,12 +1,17 @@
+package Estudiante;
 
 public class Estudiante {
 	public String Nombre;
-	public Integer Codigo;
+	public String Codigo;
 	public Integer Nota;
 	
-	public Estudiante (String T_nombre, Integer T_codigo, Integer T_nota){
+	public Estudiante (String T_nombre, String T_codigo, Integer T_nota){
 		this.Nombre=T_nombre;
 		this.Codigo=T_codigo;
 		this.Nota=T_nota;
+	}
+	
+	public String toString(){
+		return "{" + Nombre + ", " + Codigo + ", " + Nota + "}";
 	}
 }
